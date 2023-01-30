e05e07118b22d614fca2343c323c36ba2fc32c74
Vikas_Singh <Vikas_Singh@VikasPC>
2023-01-30 17:03:36 +0530

    seven commit

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -10,6 +10,13 @@ public class Message {
     public Message() {
     }
 
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    
+    
     public Message(int id, String content, Date timestamp) {
         this.id = id;
         this.content = content;
